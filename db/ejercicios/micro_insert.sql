a0998e2e17f87bc579491107921c93513b69e1f7
Matias Yaryura <yayumetal@hotmail.com>
2017-05-17 19:16:34 -0300

    nuevos inserts

---
+++ db/ejercicios/micro_insert.sql
@@ -0,0 +1,35 @@
+﻿INSERT INTO micro (dominio, marca, modelo) VALUES
+('GDJ632', 'MERCEDES BENZ', '1526'),
+('DCH732', 'Scania', 'K 111 TC 31'),
+('HDG652', 'MERCEDES BENZ', 'O 500 M30'),
+('HWO103', 'MERCEDES BENZ', 'O 500 rsd');
+
+INSERT INTO micro (dominio, marca, modelo) VALUES
+('JKFD745', 'MERCEDES BENZ', '1954'),
+('EVC258', 'Scania', '1987'),
+('JHF764', 'Matefer', '1982'),
+('VNV289', 'Marcopolo', '1976');
+
+INSERT INTO micro (dominio, marca, modelo) VALUES
+('VDD634', 'Saldivia', '1999'),
+('IFD896', 'Marcopolo', 'K 111 TC 31'),
+('FDH345', 'Matafer', '1996'),
+('KHD653', 'MERCEDES BENZ', '1973');
+
+INSERT INTO micro (dominio, marca, modelo) VALUES
+('VCG734', 'Matafer', '1899'),
+('MJU437', 'Scania', '1994'),
+('VII643', 'MERCEDES BENZ', '1995'),
+('OLP665', 'Marcopolo', '1992');
+
+INSERT INTO micro (dominio, marca, modelo) VALUES
+('FFD688', 'MERCEDES BENZ', '1991'),
+('SCC499', 'Marcopolo', '1998'),
+('TER257', 'Meterfer', '1889'),
+('DNY519', 'MERCEDES BENZ', '1897');
+
+INSERT INTO micro (dominio, marca, modelo) VALUES
+('HGT874', 'Marcopolo', '1895'),
+('POL448', 'Mercedez Benz', '1890'),
+('BVN576', 'Scania', '1990'),
+('RWQ309', 'MERCEDES BENZ', '1997');
